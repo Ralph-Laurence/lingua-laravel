5636abc40b83e205becb08c6356b92576ecc678d
Laurence <sempailaurence@gmail.com>
2025-02-14 18:25:26 +0800

    Add Disabilities Implicit - Explicit Values

--- app/Http/Utils/Constants.php
+++ app/Http/Utils/Constants.php
@@ -21,4 +21,23 @@ class Constants {
     const DocPathEducation      = 'public/documentary_proofs/education/';
     const DocPathWorkExp        = 'public/documentary_proofs/work_experience/';
     const DocPathCertification  = 'public/documentary_proofs/certification/';
+
+    /**
+     * We dont want to hurt our users' feelings.
+     * DisabilitiesPublic is a non-offensive description
+     * whereas DisabilitiesAdmin is explicitly for admin.
+     */
+    const DisabilitiesPublic = [
+        '0' => 'No Impairments',
+        '1' => 'Deaf or Hard of Hearing',
+        '2' => 'Non-Verbal',
+        '3' => 'Both Deaf and Non-Verbal'
+    ];
+
+    const DisabilitiesAdmin = [
+        '0' => 'None',
+        '1' => 'Deaf',
+        '2' => 'Mute',
+        '3' => 'Both Mute & Deaf'
+    ];
 }
